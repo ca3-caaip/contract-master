50c979ceea7651ede08d5efa756a48d80d7e6c8b
Keisuke Iwamura <i.ksk0601@gmail.com>
2022-10-03 13:43:20 +0900

    refactor: contract_master

--- contract_master/bsc/main.py
+++ contract_master/bsc/main.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 from os import path
 from typing import Type
 
 from web3 import Web3
+
+from contract_master.common.models import CovalentTx
 
 from ..common import (
     BalanceResult,
@@ -23,29 +26,60 @@ from .contract import (
 class BscContractMaster(ContractMaster):
     MASTER_CSV_FILE_PATH = path.join(path.dirname(__file__), "./master.csv")
 
-    def __init__(self, quicknode_endpoint: str):
+    def __init__(self, quicknode_endpoint: str, txs: list[CovalentTx], target_datetime: datetime, user_address: str):
+        super().__init__(txs, target_datetime, user_address)
         self.master = load_master_data(BscContractMaster.MASTER_CSV_FILE_PATH)
         self.web3 = Web3(Web3.HTTPProvider(quicknode_endpoint))
         if not self.web3.isConnected():
             raise Exception("QuickNodeConnectionError")
 
-    def get_token_balance(
-        self, contract_address: str, user_address: str, block_height: int | None = None
-    ) -> BalanceResult | ErroredResult:
+    def get_balances(self) -> list[list[BalanceResult] | list[IgnoredResult] | list[ErroredResult]]:
+        fungible_token_balances, fungible_errors = self.__get_fungible_token_address_balances()
+        possession_balances, possesion_errors, ignored_addresses = self.__get_possessable_address_balances()
+        balance_result = fungible_token_balances + possession_balances
+        errors = fungible_errors + possesion_errors
+        return [balance_result, errors, ignored_addresses]
+
+    def __get_possessable_address_balances(
+        self,
+    ) -> tuple[list[BalanceResult], list[ErroredResult], list[IgnoredResult]]:
+        possession_balances: list[BalanceResult] = list()
+        errors: list[ErroredResult] = list()
+        ignored_addresses: list[IgnoredResult] = list()
+        for address in self.possessable_addresses:
+            res = self.__get_balance(contract_address=address)
+            if isinstance(res, IgnoredResult):
+                ignored_addresses.append(IgnoredResult(token=address))
+            elif isinstance(res, ErroredResult):
+                errors.append(res)
+            else:
+                possession_balances.append(res)
+        return (possession_balances, errors, ignored_addresses)
+
+    def __get_fungible_token_address_balances(self) -> tuple[list[BalanceResult], list[ErroredResult]]:
+        errors: list[ErroredResult] = list()
+        fungible_token_balances: list[BalanceResult] = list()
+        for address in self.fungible_token_addresses:
+            res = self.__get_token_balance(contract_address=address)
+            if isinstance(res, ErroredResult):
+                errors.append(res)
+            else:
+                fungible_token_balances.append(res)
+        return (fungible_token_balances, errors)
+
+    def __get_token_balance(self, contract_address: str) -> BalanceResult | ErroredResult:
         try:
             return BalanceResult(
                 application="bsc",
                 service="spot",
                 item=Bep20TokenContract(web3=self.web3, address=contract_address).balance_of(
-                    account=user_address, block_height=block_height
+                    account=self.user_address, block_height=self.block_height
                 ),
             )
         except Exception as e:
             return ErroredResult(token=contract_address, reason=str(e))
 
-    def get_balance(
-        self, contract_address: str, user_address: str, block_height: int | None = None
-    ) -> BalanceResult | IgnoredResult | ErroredResult:
+    def __get_balance(self, contract_address: str) -> BalanceResult | IgnoredResult | ErroredResult:
         contract: Type[Contract]
         master = self.master.get(contract_address, None)
         if master is None:
@@ -70,7 +104,7 @@ class BscContractMaster(ContractMaster):
                 application=master.application,
                 service=master.service,
                 item=contract(web3=self.web3, address=contract_address).balance_of(
-                    account=user_address, block_height=block_height
+                    account=self.user_address, block_height=self.block_height
                 ),
             )
         except Exception as e:
--- contract_master/common/contract_master.py
+++ contract_master/common/contract_master.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Callable
 
 from pydantic.main import BaseModel
 
-from .models import ServiceItem
+from .models import CovalentTx, ServiceItem
+from .utils import equals, lower, unique
 
 
 class BalanceResult(BaseModel):
@@ -35,18 +38,49 @@ class ErroredResult(BaseModel):
 class ContractMaster(ABC):
     MASTER_CSV_FILE_PATH: str
 
-    @abstractmethod
-    def __init__(self) -> None:
+    def __init__(self, txs: list[CovalentTx], target_datetime: datetime, user_address: str) -> None:
+        self.user_address = user_address
+        self.txs = list(filter(self.__is_transaction_within(target_datetime), txs))
+        self.block_height = self.__get_max_block_height(self.txs)
+        fungible_token_addresses, possessable_addresses = self.__get_relevant_contract_addresses(
+            base_address=user_address, transactions=self.txs
+        )
+        self.fungible_token_addresses = fungible_token_addresses
+        self.possessable_addresses = possessable_addresses
         pass
 
-    @abstractmethod
-    def get_token_balance(
-        self, contract_address: str, user_address: str, block_height: int | None = None
-    ) -> BalanceResult | IgnoredResult | ErroredResult:
-        pass
+    def __is_transaction_within(self, max_datetime: datetime) -> Callable[[CovalentTx], bool]:
+        return lambda x: x.block_signed_at <= max_datetime
+
+    def __get_max_block_height(self, transactions: list[CovalentTx]) -> int:
+        if len(transactions) <= 0:
+            raise Exception("NoTransactionsPassed")  # TODO: エラーハンドル
+        max_block_height: int = 0
+        for tx in transactions:
+            if tx.block_height > max_block_height:
+                max_block_height = tx.block_height
+        return max_block_height
 
     @abstractmethod
-    def get_balance(
-        self, contract_address: str, user_address: str, block_height: int | None = None
-    ) -> BalanceResult | IgnoredResult | ErroredResult:
+    def get_balances(self) -> list[list[BalanceResult] | list[IgnoredResult] | list[ErroredResult]]:
         pass
+
+    def __get_relevant_contract_addresses(
+        self, base_address: str, transactions: list[CovalentTx]
+    ) -> tuple[list[str], list[str]]:
+        fungible_token_addresses: list[str] = []
+        possessable_addresses: list[str] = []
+
+        for tx in transactions:
+            for e in tx.log_events:
+                if e.decoded and e.decoded.name == "Transfer":
+                    # 自分が含まれるTransferイベントのsender_addressはfungible tokenとして全て収集し、資産取得対象に含める
+                    if equals(e.decoded.get_param("from"), base_address) or equals(
+                        e.decoded.get_param("to"), base_address
+                    ):
+                        fungible_token_addresses.append(e.sender_address)
+                    # 自分からどこかのコントラクトにTransferしているものはpossessableとみなし、資産取得対象に含める
+                    if equals(e.decoded.get_param("from"), base_address):
+                        possessable_addresses.append(e.decoded.get_param("to"))
+
+        return unique(lower(fungible_token_addresses)), unique(lower(possessable_addresses))
