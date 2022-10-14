c12ebdf7394b8a81d487577419615485e7ebc350
Keisuke Iwamura <i.ksk0601@gmail.com>
2022-10-14 15:41:22 +0900

    fixup! feat: add polygon contracat master

--- contract_master/common/models.py
+++ contract_master/common/models.py
@@ -64,7 +64,7 @@ class LiquidityPoolServiceItem(BaseModel):
         supply: list[TokenAmount]
         description: Optional[str] = None
 
-    type: Literal["liquidity_pool"] = "liquidity_pool"
+    type: Literal["liquidity pool"] = "liquidity pool"
     data: LiquidityPoolServiceData
 
     def is_empty(self) -> bool:
